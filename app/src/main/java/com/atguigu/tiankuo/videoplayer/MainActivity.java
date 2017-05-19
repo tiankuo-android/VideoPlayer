1dfb657faaee568b9e33a499bd3f94668288b4bf
tiankuo-android <1226147264@qq.com>
2017-05-19 17:04:41 +0800

    第七次修改代码-视频列表显示&&把视频放入模拟器中

--- app/src/main/java/com/atguigu/tiankuo/videoplayer/MainActivity.java
+++ app/src/main/java/com/atguigu/tiankuo/videoplayer/MainActivity.java
@@ -1,9 +1,14 @@
 package com.atguigu.tiankuo.videoplayer;
 
+import android.Manifest;
+import android.app.Activity;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.widget.RadioGroup;
 
 import com.atguigu.tiankuo.videoplayer.fragment.BaseFragment;
@@ -24,14 +29,19 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Log.e("MainActivity", "onCreate");
         setContentView(R.layout.activity_main);
+        isGrantExternalRW(this);
         //初始化控件
-        rg_main = (RadioGroup)findViewById(R.id.rg_main);
+        rg_main = (RadioGroup) findViewById(R.id.rg_main);
         initFragment();
         //设置监听
         rg_main.setOnCheckedChangeListener(new MyOnCheckedChangeListener());
+        //默认选择本地视频
         rg_main.check(R.id.rb_local_video);
     }
+
+
 
     private void initFragment() {
         //把各个页面实例化放入集合中
@@ -42,11 +52,11 @@ public class MainActivity extends AppCompatActivity {
         fragments.add(new NetVideoPager());
     }
 
-    class MyOnCheckedChangeListener implements RadioGroup.OnCheckedChangeListener{
+    class MyOnCheckedChangeListener implements RadioGroup.OnCheckedChangeListener {
 
         @Override
         public void onCheckedChanged(RadioGroup group, int checkedId) {
-            switch (checkedId){
+            switch (checkedId) {
                 case R.id.rb_local_video:
                     position = 0;
                     break;
@@ -59,7 +69,7 @@ public class MainActivity extends AppCompatActivity {
                     break;
 
                 case R.id.rb_net_video:
-                    position  = 3;
+                    position = 3;
                     break;
             }
             //根据位置得到对应的Fragment
@@ -70,8 +80,8 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-    private void addFragment(BaseFragment currentFragment) {
-        if(tempFragment != currentFragment) {
+    private void addFragment(Fragment currentFragment) {
+        if (tempFragment != currentFragment) {
             FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
             if (!currentFragment.isAdded()) {
                 if (tempFragment != null) {
@@ -79,13 +89,60 @@ public class MainActivity extends AppCompatActivity {
                 }
                 ft.add(R.id.fl_content, currentFragment);
             } else {
-                if(tempFragment != null) {
+                if (tempFragment != null) {
                     ft.hide(tempFragment);
                 }
                 ft.show(currentFragment);
             }
             ft.commit();
+            tempFragment = currentFragment;
         }
-        tempFragment = currentFragment;
+    }
+    public static boolean isGrantExternalRW(Activity activity) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M && activity.checkSelfPermission(
+                Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+
+            activity.requestPermissions(new String[]{
+                    Manifest.permission.READ_EXTERNAL_STORAGE,
+                    Manifest.permission.WRITE_EXTERNAL_STORAGE
+            }, 1);
+            return false;
+        }
+        return true;
+    }
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.e("MainActivity", "onRestart");
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.e("MainActivity", "onStart");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.e("MainActivity", "onResume");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.e("MainActivity", "onPause");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.e("MainActivity", "onStop");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.e("MainActivity", "onDestroy");
     }
 }
--- app/src/main/java/com/atguigu/tiankuo/videoplayer/pager/LocalVideoPager.java
+++ app/src/main/java/com/atguigu/tiankuo/videoplayer/pager/LocalVideoPager.java
@@ -1,11 +1,24 @@
 package com.atguigu.tiankuo.videoplayer.pager;
 
-import android.graphics.Color;
-import android.view.Gravity;
+import android.content.ContentResolver;
+import android.database.Cursor;
+import android.net.Uri;
+import android.os.Handler;
+import android.os.Message;
+import android.provider.MediaStore;
+import android.util.Log;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.atguigu.tiankuo.videoplayer.R;
+import com.atguigu.tiankuo.videoplayer.adapter.LocalVideoAdapter;
+import com.atguigu.tiankuo.videoplayer.domain.MediaItem;
 import com.atguigu.tiankuo.videoplayer.fragment.BaseFragment;
+
+import java.util.ArrayList;
 
 /**
  * 作者：杨光福 on 2017/5/19 11:47
@@ -15,21 +28,96 @@ import com.atguigu.tiankuo.videoplayer.fragment.BaseFragment;
  */
 
 public class LocalVideoPager extends BaseFragment {
-    private TextView textView;
 
-    //重写视图
+    private ListView lv;
+    private TextView tv_nodata;
+    private ArrayList<MediaItem> mediaItems;
+    private LocalVideoAdapter adapter;
+
+    //重写视图-返回View
     @Override
     public View initView() {
-        textView = new TextView(context);
-        textView.setTextSize(30);
-        textView.setGravity(Gravity.CENTER);
-        textView.setTextColor(Color.RED);
-        return textView;
+        Log.e("TAG","LocalVideoPager-initView");
+        View  view = View.inflate(context, R.layout.fragment_local_vidio,null);
+        lv = (ListView) view.findViewById(R.id.lv_vidio);
+        tv_nodata = (TextView) view.findViewById(R.id.tv_content);
+        //设置item的点击事件
+        lv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                //得到点击item对应的对象
+//                MediaItem mediaItem = mediaItems.get(position);
+
+                MediaItem item = adapter.getItem(position);
+                Toast.makeText(context, ""+item.toString(), Toast.LENGTH_SHORT).show();
+
+
+            }
+        });
+        return view;
     }
 
     @Override
     public void initData() {
         super.initData();
-        textView.setText("本地视频的内容");
+        Log.e("TAG","LocalVideoPager-initData");
+        //加载本地所有的视频
+        getData();
+
+
+    }
+
+    private Handler handler = new Handler(){
+        @Override
+        public void handleMessage(Message msg) {
+            super.handleMessage(msg);
+
+            if(mediaItems != null && mediaItems.size() >0){
+                //有数据
+                tv_nodata.setVisibility(View.GONE);
+                //设置适配器
+                adapter = new LocalVideoAdapter(context,mediaItems);
+                lv.setAdapter(adapter);
+            }else{
+                //没有数据
+                tv_nodata.setVisibility(View.VISIBLE);
+            }
+        }
+    };
+
+    /**
+     * 得到数据
+     */
+    private void getData() {
+        new Thread(){
+            public void run(){
+                mediaItems = new ArrayList<MediaItem>();
+                ContentResolver resolver = context.getContentResolver();
+                Uri uri = MediaStore.Video.Media.EXTERNAL_CONTENT_URI;
+                String[] objs = {
+                        MediaStore.Video.Media.DISPLAY_NAME,//视频在sdcard上的名称
+                        MediaStore.Video.Media.DURATION,//视频时长
+                        MediaStore.Video.Media.SIZE,//视频文件的大小
+                        MediaStore.Video.Media.DATA//视频播放地址
+                };
+                Cursor cursor = resolver.query(uri, objs, null, null, null);
+                if(cursor != null ){
+                    while (cursor.moveToNext()){
+                        String name = cursor.getString(cursor.getColumnIndex(MediaStore.Video.Media.DISPLAY_NAME));
+                        long duration = cursor.getLong(cursor.getColumnIndex(MediaStore.Video.Media.DURATION));
+                        long size = cursor.getLong(cursor.getColumnIndex(MediaStore.Video.Media.SIZE));
+                        String data = cursor.getString(cursor.getColumnIndex(MediaStore.Video.Media.DATA));
+                        Log.e("TAG","name=="+name+",duration=="+duration+",data==="+data);
+
+                        mediaItems.add(new MediaItem(name,duration,size,data));
+
+                        //使用handler
+                        handler.sendEmptyMessage(0);
+                    }
+
+                    cursor.close();
+                }
+            }
+        }.start();
     }
 }
