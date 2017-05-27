f3e3ed96f4ee404bb99269fd2d29d626d4c5d4bd
tiankuo-android <1226147264@qq.com>
2017-05-27 11:45:14 +0800

    第五十八次修改代码--增加软件退出功能

---
+++ app/src/main/java/com/atguigu/tiankuo/videoplayer/activity/SearchActivity.java
@@ -0,0 +1,15 @@
+package com.atguigu.tiankuo.videoplayer.activity;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.atguigu.tiankuo.videoplayer.R;
+
+public class SearchActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_search);
+    }
+}
--- app/src/main/java/com/atguigu/tiankuo/videoplayer/view/Titlebar.java
+++ app/src/main/java/com/atguigu/tiankuo/videoplayer/view/Titlebar.java
@@ -1,6 +1,7 @@
 package com.atguigu.tiankuo.videoplayer.view;
 
 import android.content.Context;
+import android.content.Intent;
 import android.util.AttributeSet;
 import android.view.View;
 import android.widget.ImageView;
@@ -10,6 +11,7 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import com.atguigu.tiankuo.videoplayer.R;
+import com.atguigu.tiankuo.videoplayer.activity.SearchActivity;
 
 public class Titlebar extends LinearLayout implements View.OnClickListener {
 
@@ -38,7 +40,9 @@ public class Titlebar extends LinearLayout implements View.OnClickListener {
     public void onClick(View v) {
         switch (v.getId()){
             case R.id.tv_sousuo:
-                Toast.makeText(context, "搜索", Toast.LENGTH_SHORT).show();
+//                Toast.makeText(context, "搜索", Toast.LENGTH_SHORT).show();
+                Intent intent = new Intent(context,SearchActivity.class);
+                context.startActivity(intent);
                 break;
             case R.id.rl_game:
                 Toast.makeText(context, "游戏", Toast.LENGTH_SHORT).show();
